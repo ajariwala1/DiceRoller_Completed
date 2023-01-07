714bc911c311367e004da08fe1c3df771053c479
Abhishek Jariwala <ajariwala@auburn.edu>
2023-01-07 09:09:39 -0600

    part1

--- DiceRoller_Completed/ViewController.swift
+++ DiceRoller_Completed/ViewController.swift
@@ -7,11 +7,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageTwo: UIImageView!
+    @IBOutlet weak var diceImageOne: UIImageView!
+    let imagesArray = ["1", "2", "3", "4", "5", "6"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func diceRolled(_ sender: UIButton) {
+        let randomOne = imagesArray.randomElement()
+        let randomTwo = imagesArray.randomElement()
+        diceImageOne.image = UIImage(named: randomOne!)
+        diceImageTwo.image = UIImage(named: randomTwo!)
+        
+    }
 }
 
